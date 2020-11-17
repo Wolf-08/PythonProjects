b5c3a2efab4c431434dabfe8b85e510e27301479
Alejandro <alejandro.montecillo@gmail>
2020-11-17 14:20:37 -0600

    :memo: Add : Some ways to print out a dictionary

--- Anexos/sets.py
+++ Anexos/sets.py
@@ -14,3 +14,25 @@ print(planetas)
 #Para limpiar el ser 
 planetas.clear()
 print(planetas)
+
+
+dicc= {
+  "D1": "Dato1",
+  "D2": "Dato2",
+  "D3": "Dato3"
+}
+
+#Ways to print out a element in a dictionary 
+#1. Show the key of dictionary
+for element in dicc:
+  print(element)
+#2. Show the value of the key 
+for element in dicc:
+  print(dicc[element])
+#3. Show the values trohugt the value 
+for element in dicc.values():
+  print(element)
+#4.Show the key and the value
+for key in dicc:
+  print(key,"->",dicc[key], end=" ,")
+
